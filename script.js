f09a1889a60a95b8d5092ee97598a6911b57a229
Siddharth Chattopadhyay <siddh@Nabanitas-MacBook-Air.local>
2025-11-26 09:55:36 +0530

    pending

---
+++ script.js
@@ -0,0 +1,11 @@
+const tasks = [];
+
+class Task{
+
+    createTask(newUId, title, description, category, due_date, status){
+        
+    }
+    updateTask(UId, title, description, category, due_date, status){}
+    deleteTask(UId){}
+    markComplete(UId){}
+}
